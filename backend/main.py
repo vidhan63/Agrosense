ec756846cc512d5341e36d95f9e013d5b64e47ec
auspy <vinayak.kshetez0102@gmail.com>
2024-04-18 14:50:25 +0530

    model2

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,10 @@ def recommendation(N, P, K, temperature, humidity, ph, rainfall):
     features = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
     prediction = rfc.predict(features)
     return prediction[0]
-
+def fertiPredict(Temparature,Humidity,Moisture,Nitrogen,Potassium,Phosphorous,Soil_Type,Crop_Type):
+    fetriModel = joblib.load("ferti.joblib")
+    prediction = fetriModel.predict([[Temparature,Humidity,Moisture,Nitrogen,Potassium,Phosphorous,Soil_Type,Crop_Type]])
+    return prediction
 
 @app.route("/")
 def index():
@@ -25,13 +28,52 @@ def index():
 def health():
     return "healthy"
 
-
+@app.route("/ferti",methods=["GET","POST"])
+def ferti():
+    try:
+        res = ""
+        if request.method == "POST":
+            data = request.get_json(force=True)
+            # Extract feature values from the JSON data
+            temperature = float(data.get("Temperature", 40))
+            humidity = int(data.get("Humidity", 20))
+            moisture = int(data.get("Moisture", 30))
+            nitrogen = int(data.get("Nitrogen", 20))
+            potassium = int(data.get("Potassium", 30))
+            phosphorous = int(data.get("Phosphorous", 40))
+            soil_type = data.get("Soil_Type", "Loam")  # Assuming default as "Loam" if not provided
+            crop_type = data.get("Crop_Type", "Rice")  # Assuming default as "Rice" if not provided
+            ans = fertiPredict(temperature,humidity,moisture,nitrogen,potassium,phosphorous,soil_type,crop_type)
+            if ans[0] == 0:
+                print("10-26-26")
+                res = "10-26-26"
+            elif ans[0] ==1:
+                print("14-35-14")
+                res = "14-35-14"
+            elif ans[0] == 2:
+                print("17-17-17")
+                res = "17-17-17"
+            elif ans[0] == 3:
+                print("20-20")
+                res = "20-20"
+            elif ans[0] == 4:
+                print("28-28")
+                res = "28-28"
+            elif ans[0] == 5:
+                print("DAP")
+                res = "DAP"
+            else:
+                print("Urea")
+                res = "Urea"
+        if res == "":
+            return jsonify({"error": "Invalid data received!"})
+        return jsonify({"fertilizer": res})
+    except Exception as e:
+        return jsonify({"error": "Invalid data received!"})
 # eg: predict?N=20&P=30&K=40&temperature=40.0&humidity=20&ph=30&rainfall=50
 @app.route("/predict", methods=["GET", "POST"])
 def predict():
     try:
-        print("here")
-        print(request.args.to_dict())
         if request.method == "GET":
             # Handling GET request
             print("here")
